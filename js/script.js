a4fa955dd287d590a1f5cd0ba35bbe37d2f0f22d
Dmitry <65062995+Dimkabit@users.noreply.github.com>
2021-09-26 07:51:40 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,4 +13,50 @@ headerBerger.addEventListener('click', (e) => {
 	
 });
 
+$(function () {
+	$(window).scroll(function() {
+		$('.second').each(function(){
+			 var imagePos = $(this).offset().top;
+
+			 var topOfWindow = $(window).scrollTop();
+			 if (imagePos < topOfWindow+650) {
+				  $(this).addClass("fadeInUp");
+			 }
+		});
+  });
+  
+	$(window).scroll(function() {
+	    $('.third').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.first').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+
+	$(window).scroll(function() {
+		$('.zumIn').each(function(){
+			 var imagePos = $(this).offset().top;
+
+			 var topOfWindow = $(window).scrollTop();
+			 if (imagePos < topOfWindow+1000) {
+				  $(this).addClass("zoomIn");
+			 }
+		});
+  });
+  
+})
+
 
